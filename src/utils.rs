6552abb4b080c580fff6dcddf7f6113e22a8bfb3
Gourab-Ghosh <gourab200ghosh@gmail.com>
2023-03-02 03:45:36 +0530

    M  src/utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,10 @@ pub mod command_utils {
     use std::io::Write;
 
     pub fn input<T: std::fmt::Display>(q: T) -> String {
-        print!("{q}");
-        std::io::stdout().flush().unwrap();
+        if !q.to_string().is_empty() {
+            print!("{q}");
+            std::io::stdout().flush().unwrap();
+        }
         let mut user_input = String::new();
         std::io::stdin()
             .read_line(&mut user_input)
