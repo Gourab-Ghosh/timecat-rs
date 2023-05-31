641bbd65802d3601836e220a131289adcdb17262
Gourab-Ghosh <gourab200ghosh@gmail.com>
2023-06-01 01:05:55 +0530

    A  src/lib.rs, M  src/main.rs

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+
+mod board;
+mod constants;
+mod engine;
+mod evaluate;
+mod nnue;
+mod nnue_rs;
+mod nnue_weights;
+mod parse;
+mod sort;
+mod timer;
+mod tt;
+mod useful_macros;
+mod utils;
+
+pub use board::*;
+pub use chess::Color::*;
+pub use chess::Piece::*;
+pub use chess::{
+    get_adjacent_files, get_bishop_moves, get_file as get_file_bb, get_king_moves,
+    get_knight_moves, get_pawn_attacks, get_rank as get_rank_bb, get_rook_moves, BitBoard,
+    BoardStatus, CacheTable, ChessMove as Move, Color, File, Piece, Rank, Square,
+};
+pub use constants::bitboard::*;
+pub use constants::board_representation::*;
+pub use constants::engine_constants::*;
+pub use constants::fen::*;
+pub use constants::print_style::*;
+pub use constants::square::*;
+pub use constants::types::*;
+pub use constants::VERSION;
+pub use engine::{Engine, GoCommand};
+pub use evaluate::*;
+pub use failure::Fail;
+pub use fxhash::FxHashMap as HashMap;
+pub use itertools::Itertools;
+pub use parse::*;
+pub use sort::*;
+pub use std::cmp::{self, Ordering};
+pub use std::convert::From;
+pub use std::env;
+pub use std::fmt::{self, Display};
+pub use std::io::{self, Write};
+pub use std::mem;
+pub use std::num::ParseIntError;
+pub use std::str::FromStr;
+pub use std::str::ParseBoolError;
+pub use std::sync::{Arc, Mutex};
+pub use std::time::{Duration, Instant};
+pub use timer::Timer;
+pub use tt::*;
+pub use useful_macros::*;
+pub use utils::bitboard_utils::*;
+pub use utils::cache_table_utils::*;
+pub use utils::classes::*;
+pub use utils::common_utils::*;
+pub use utils::square_utils::*;
+pub use utils::string_utils::*;
+pub use utils::unsafe_utils::*;
--- src/main.rs
+++ src/main.rs
@@ -4,71 +4,12 @@
 // https://github.com/topics/chess-engine?l=rust
 // https://github.com/dsekercioglu/blackmarlin.git
 // https://github.com/zxqfl/sashimi
+// https://backscattering.de/chess/uci/
 
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_macros)]
-
-mod board;
-mod constants;
-mod engine;
-mod evaluate;
-mod nnue;
-mod nnue_rs;
-mod nnue_weights;
-mod parse;
-mod sort;
 mod tests;
-mod timer;
-mod tt;
-mod useful_macros;
-mod utils;
 
-use board::*;
-use chess::Color::*;
-use chess::Piece::*;
-use chess::{
-    get_adjacent_files, get_bishop_moves, get_file as get_file_bb, get_king_moves,
-    get_knight_moves, get_pawn_attacks, get_rank as get_rank_bb, get_rook_moves, BitBoard,
-    BoardStatus, CacheTable, ChessMove as Move, Color, File, Piece, Rank, Square,
-};
-use constants::bitboard::*;
-use constants::board_representation::*;
-use constants::engine_constants::*;
-use constants::fen::*;
-use constants::print_style::*;
-use constants::square::*;
-use constants::types::*;
-use constants::VERSION;
-use engine::{Engine, GoCommand};
-use evaluate::*;
-use failure::Fail;
-use fxhash::FxHashMap as HashMap;
-use itertools::Itertools;
-use parse::*;
-use sort::*;
-use std::cmp::{self, Ordering};
-use std::convert::From;
-use std::env;
-use std::fmt::{self, Display};
-use std::io::{self, Write};
-use std::mem;
-use std::num::ParseIntError;
-use std::str::FromStr;
-use std::str::ParseBoolError;
-use std::sync::{Arc, Mutex};
-use std::time::{Duration, Instant};
+use timecat::*;
 use tests::test;
-use timer::Timer;
-use tt::*;
-use useful_macros::*;
-use utils::bitboard_utils::*;
-use utils::cache_table_utils::*;
-use utils::classes::*;
-use utils::common_utils::*;
-use utils::square_utils::*;
-use utils::string_utils::*;
-use utils::unsafe_utils::*;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
