6feda96d23ec9c2a229f3ceb0ba3b81866f047f2
Gourab-Ghosh <gourab200ghosh@gmail.com>
2024-05-20 18:33:46 +0530

    M  src/useful_macros.rs

--- src/useful_macros.rs
+++ src/useful_macros.rs
@@ -1,24 +1,3 @@
-#[macro_export]
-macro_rules! generator {
-    ($id1:ident | $id2:ident in $it:expr) => {{
-        let mut vec = Vec::new();
-        for $id2 in $it {
-            vec.push($id1);
-        }
-        vec
-    }};
-
-    ($id1:ident | $id2:ident in $it:expr, $cond:expr) => {{
-        let mut vec = Vec::new();
-        for $id2 in $it {
-            if $cond {
-                vec.push($id1);
-            }
-        }
-        vec
-    }};
-}
-
 #[cfg(debug_assertions)]
 #[macro_export]
 macro_rules! get_item_unchecked {
