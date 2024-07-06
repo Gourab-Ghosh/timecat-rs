f39f8053d6761a6f09d543779f5868b0a8e21adf
Gourab-Ghosh <gourab200ghosh@gmail.com>
2024-07-06 18:13:16 +0530

    M  README.md, M  manager/test.py

--- manager/test.py
+++ manager/test.py
@@ -1,8 +1,13 @@
 import os
 from .build import run_command_with_target_cpu_native
+from .constants import CARGO_TOML_FILE_DATA
 
 def test_package(release_mode = False):
+    features = list(CARGO_TOML_FILE_DATA["features"].keys())
+    features.remove("wasm")
+    features.sort()
+    features = ",".join(features)
     if release_mode:
-        return bool(run_command_with_target_cpu_native(f"cargo test --all-features --release"))
+        return bool(run_command_with_target_cpu_native(f"cargo test --no-default-features --features {features} --release"))
     else:
-        return bool(os.system("cargo test --all-features"))
+        return bool(os.system(f"cargo test --no-default-features --features {features}"))
