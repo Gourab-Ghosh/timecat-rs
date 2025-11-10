3291992eaae7687ae3ce77f0b2a4bdee78e2e6dc
Gourab-Ghosh <gg8576.work@gmail.com>
2025-11-10 18:16:50 +0530

    small changes

--- src/useful_macros.rs
+++ src/useful_macros.rs
@@ -175,17 +175,20 @@ macro_rules! println_wasm {
 
 #[macro_export]
 macro_rules! write_unchecked {
-    ($($arg:tt)*) => {
-        unsafe { write!($($arg)*).unwrap_unchecked() }
-    };
+    ($($arg:tt)*) => {{
+        let output = write!($($arg)*);
+        unsafe { output.unwrap_unchecked() }
+    }};
 }
 
 #[macro_export]
 macro_rules! writeln_unchecked {
-    () => {
-        unsafe { writeln!().unwrap_unchecked() }
-    };
-    ($($arg:tt)*) => {
-        unsafe { writeln!($($arg)*).unwrap_unchecked() }
-    };
+    () => {{
+        let output = writeln!();
+        unsafe { output.unwrap_unchecked() }
+    }};
+    ($($arg:tt)*) => {{
+        let output = writeln!($($arg)*);
+        unsafe { output.unwrap_unchecked() }
+    }};
 }
